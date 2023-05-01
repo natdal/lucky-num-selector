f4c6e07631854ee4ed7b1068ce3e6b3a69dd6b94
natdal <natdal7535@gmail.com>
2023-05-01 14:26:57 +0900

    feat: Change to extract as many sets of 6 numbers as the user wants.
    - infinite

--- selector.py
+++ selector.py
@@ -1,7 +1,11 @@
 import random
 
-numbers = []
-for i in range(6) :
-    number = random.randint(1,45)
-    numbers.append(number)
-print(numbers)
+while True :
+    num_of_sets = int(input("몇개살끼고"))
+    if num_of_sets <= 0:
+        print("몇번")
+    else:
+        for i in range(num_of_sets):
+            numbers = random.sample(range(1,46),6)
+            print(f"번호{i+1} : {sorted(numbers)}")
+        break
