60319c69dc03ee3a7db5c72ada38565c7bafa822
natdal <natdal7535@gmail.com>
2023-05-01 14:42:33 +0900

    refactor: random number functional call

--- selector.py
+++ selector.py
@@ -1,11 +1,17 @@
 import random
 
-while True :
-    num_of_sets = int(input("몇개살끼고"))
-    if num_of_sets <= 0:
-        print("몇번")
-    else:
-        for i in range(num_of_sets):
-            numbers = random.sample(range(1,46),6)
-            print(f"번호{i+1} : {sorted(numbers)}")
-        break
+#while True :
+#    num_of_sets = int(input("몇개살끼고"))
+#    if num_of_sets <= 0:
+#        print("몇번")
+#    else:
+#        for i in range(num_of_sets):
+#            numbers = random.sample(range(1,46),6)
+#            print(f"번호{i+1} : {sorted(numbers)}")
+#        break
+
+def get_lucky_nums() :
+    return random.sample(range(1,45+1), k=6)
+
+if __name__ == '__main__' :
+    print(get_lucky_nums())
